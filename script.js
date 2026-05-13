23823e436863cfc18a604c978e6ce874231cbf8f
Shubhangi Bhapkar <shubhangibhapkar35@gmail.com>
2026-05-13 23:34:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function firstWord(s) {
   // your code here
+	let first = s.splice(" ");
+	return first[0];
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
+const s = prompt("Enter String:"); 
 alert(firstWord(s));
