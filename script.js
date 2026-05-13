10cca63969073a65d0e25b2ff7605546862d6277
Shubhangi Bhapkar <shubhangibhapkar35@gmail.com>
2026-05-14 00:08:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 function firstWord(s) {
   // your code here
-	if(s = " "){
-		return s;
-	}
 	const first = s.match(/^\w+/)?.[0];
+	
 	return first;
 }
-
+ 
 // Do not change the code below
 
 const s = prompt("Enter String:"); 
